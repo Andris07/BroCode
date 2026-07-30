eec84d2ec730bab8a46f111efae58d8a627f46a2
Andris <laczkovicsandris07.education@gmail.com>
2026-07-30 13:03:43 +0200

    Bro Code C# - 4. constants

---
+++ Bro Code Full Courses/C# Full Course/4. constants/Program.cs
@@ -0,0 +1,8 @@
+﻿// constants = immutable values which are known at compile time
+//             and do not change for the life of the program
+
+const double PI = 3.14159;
+
+Console.WriteLine(PI);
+
+Console.ReadKey();
