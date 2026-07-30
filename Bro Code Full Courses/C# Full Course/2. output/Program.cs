6b63f367ac0bf66382dc5f3635ddab810df25ab3
Andris <laczkovicsandris07.education@gmail.com>
2026-07-30 12:38:23 +0200

    Bro Code C# - 2. output project

---
+++ Bro Code Full Courses/C# Full Course/2. output/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Hey!");
+Console.WriteLine("Hello!");
+
+// This is a comment
+
+/*
+ * this
+ * is
+ * a
+ * multiline
+ * comment
+*/
+
+Console.WriteLine("BroCode");
+
+Console.ReadKey();
