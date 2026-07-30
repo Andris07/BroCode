55d9c6ff4ae37abf14d0b00f3276381142722d6f
Andris <laczkovicsandris07.education@gmail.com>
2026-07-30 13:01:16 +0200

    Bro Code C# - 3. variables project

---
+++ Bro Code Full Courses/C# Full Course/3. variables/Program.cs
@@ -0,0 +1,24 @@
+﻿int x; // declaration
+x = 123; // initialization
+
+int y = 321; // declaration + initialization
+
+int z = x + y;
+
+int age = 21; // whole integer
+double height = 300.5; // decimal number
+bool alive = true;
+char symbol = '@';
+string name = "Bro";
+
+Console.WriteLine("Hello " + name);
+Console.WriteLine("Your age is " + age);
+Console.WriteLine("Your height is " + height + "cm");
+Console.WriteLine("Are you alive? " + alive);
+Console.WriteLine("Your symbol is: " + symbol);
+
+string userName = symbol + name;
+
+Console.WriteLine("Your username is: " + userName);
+
+Console.ReadKey();
