e4094144b888bf1308baf4b85e6a379f4d206d05
Andris <laczkovicsandris07.education@gmail.com>
2026-07-30 13:35:16 +0200

    Bro Code C# - 6. user input project

---
+++ Bro Code Full Courses/C# Full Course/6. user input/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("What's your name? ");
+string name = Console.ReadLine()!;
+
+Console.Write("What's your age? ");
+int age = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Hello " + name);
+Console.WriteLine("You are " + age + " years old");
