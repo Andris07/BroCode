8be3cdd0be75e3b941f20f8b7bb332ce11965bbb
Andris <laczkovicsandris07.education@gmail.com>
2026-07-30 13:58:14 +0200

    Bro Code C# - 8. math class project

---
+++ Bro Code Full Courses/C# Full Course/8. math class/Program.cs
@@ -0,0 +1,23 @@
+﻿double x = 3;
+const double PI = 3.14159;
+double y = 5;
+
+double a = Math.Pow(x, 3);
+double b = Math.Sqrt(x);
+double c = Math.Abs(x);
+double d = Math.Round(PI);
+double e = Math.Ceiling(PI);
+double f = Math.Floor(PI);
+double g = Math.Max(x, y);
+double h = Math.Min(x, y);
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(f);
+Console.WriteLine(g);
+Console.WriteLine(h);
+
+Console.ReadKey();
