17e9344f2dcf5d9e4169c1a80f72fa24f6840142
Andris <laczkovicsandris07.education@gmail.com>
2026-08-03 22:15:42 +0200

    Bro Code C# - 17. nested loops

---
+++ Bro Code Full Courses/C# Full Course/17. nested loops/Program.cs
@@ -0,0 +1,22 @@
+﻿// nested loops = loops inside of other loops
+//                Uses vary. Used a lot in sorting algorithms
+
+Console.Write("How many rows? ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.Write("How many columns? ");
+int columns = int.Parse(Console.ReadLine()!);
+
+Console.Write("What symbol? ");
+string symbol = Console.ReadLine()!;
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write(symbol);
+    }
+    Console.WriteLine();
+}
+
+Console.ReadKey();
