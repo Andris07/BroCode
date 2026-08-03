76f2700fed57881c4cc85bdeb6bf5b70549990dd
Andris <laczkovicsandris07.education@gmail.com>
2026-08-03 21:26:49 +0200

    Bro Code C# - 12. if statements

---
+++ Bro Code Full Courses/C# Full Course/12. if statements/Program.cs
@@ -0,0 +1,44 @@
+﻿// if statement = a basic form of decision making
+
+Console.Write("Please enter your age: ");
+int age = int.Parse(Console.ReadLine()!);
+
+if (age > 100)
+{
+    Console.WriteLine("You are too old to sign up!");
+}
+else if (age >= 18)
+{
+    Console.WriteLine("You are now signed up!");
+}
+else if (age < 0)
+{
+    Console.WriteLine("You haven't been born yet!");
+}
+else
+{
+    Console.WriteLine("You must be 18+ to sign up!");
+}
+
+Console.Write("Please enter your name: ");
+string name = Console.ReadLine()!;
+
+if (name == "")
+{
+    Console.WriteLine("You did not enter your name!");
+}
+else
+{
+    Console.WriteLine("Hello " + name);
+}
+
+//if (name != "")
+//{
+//    Console.WriteLine("Hello " + name);
+//}
+//else
+//{
+//    Console.WriteLine("You did not enter your name!");
+//}
+
+Console.ReadKey();
