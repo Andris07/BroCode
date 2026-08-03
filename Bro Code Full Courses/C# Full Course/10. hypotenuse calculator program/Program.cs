8eae21edc4a767b5264227262d96765dbe47a395
Andris <laczkovicsandris07.education@gmail.com>
2026-08-03 11:31:11 +0200

    Bro Code C# - 10. hypotenuse calculator program

---
+++ Bro Code Full Courses/C# Full Course/10. hypotenuse calculator program/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter side A: ");
+double a = double.Parse(Console.ReadLine()!);
+
+Console.Write("Enter side B: ");
+double b = double.Parse(Console.ReadLine()!);
+
+double c = Math.Sqrt((a * a) + (b * b));
+
+Console.WriteLine("The hypotenuse is: " + c);
+
+Console.ReadKey();
