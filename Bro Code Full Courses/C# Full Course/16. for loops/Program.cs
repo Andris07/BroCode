ff60eb5e82aad5fbf97897d4300cfe93a44cf234
Andris <laczkovicsandris07.education@gmail.com>
2026-08-03 21:56:43 +0200

    Bro Code C# - 16. for loops

---
+++ Bro Code Full Courses/C# Full Course/16. for loops/Program.cs
@@ -0,0 +1,15 @@
+﻿// for loop = repeats some code a FINITE amount of times
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine();
+
+for (int i = 10; i > 0; i--)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine("HAPPY NEW YEAR!");
+
+Console.ReadKey();
