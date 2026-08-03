97f13286153e28bac3f4293beeccb583f0c19c6d
Andris <laczkovicsandris07.education@gmail.com>
2026-08-03 21:47:49 +0200

    Bro Code C# - 14. logical operators

---
+++ Bro Code Full Courses/C# Full Course/14. logical operators/Program.cs
@@ -0,0 +1,18 @@
+﻿// logical operators = Can be used to check if more than 1 condition is true/false
+
+// && (AND)
+// || (OR)
+
+Console.Write("What's the temperature outside? (C°): ");
+double temp = double.Parse(Console.ReadLine()!);
+
+if (temp >= 10 && temp <= 25)
+{
+    Console.WriteLine("It's warm outside!");
+}
+else if (temp <= -50 || temp >= 50)
+{
+    Console.WriteLine("DO NOT GO OUTSIDE!");
+}
+
+Console.ReadKey();
