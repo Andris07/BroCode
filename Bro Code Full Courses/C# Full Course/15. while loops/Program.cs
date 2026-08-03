e752baab022596921ac1434068b6383d3907cb24
Andris <laczkovicsandris07.education@gmail.com>
2026-08-03 21:51:18 +0200

    Bro Code C# - 15. while loops

---
+++ Bro Code Full Courses/C# Full Course/15. while loops/Program.cs
@@ -0,0 +1,13 @@
+﻿// while loop = repeats some code while some condition remains true
+
+Console.Write("Enter your name: ");
+string name = Console.ReadLine()!;
+
+while (name == "")
+{
+    Console.Write("Enter your name: ");
+    name = Console.ReadLine()!;
+}
+Console.WriteLine("Hello " + name);
+
+Console.ReadKey();
