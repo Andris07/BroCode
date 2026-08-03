5cd3f2bfab4af1bbd92f091aef5c787e6225f819
Andris <laczkovicsandris07.education@gmail.com>
2026-08-03 11:47:04 +0200

    Bro Code C# - 11. string methods

---
+++ Bro Code Full Courses/C# Full Course/11. string methods/Program.cs
@@ -0,0 +1,22 @@
+﻿string fullName = "Bro Code";
+string phoneNumber = "123-456-7890";
+
+// fullName = fullName.ToUpper();
+// fullName = fullName.ToLower();
+// Console.WriteLine(fullName);
+
+// phoneNumber = phoneNumber.Replace("-", "/");
+// Console.WriteLine(phoneNumber);
+
+// string userName = fullName.Insert(0, "@");
+// Console.WriteLine(userName);
+
+// string firstName = fullName.Substring(0, 3);
+// string lastName = fullName.Substring(4);
+// Console.WriteLine(firstName);
+// Console.WriteLine(lastName);
+
+Console.WriteLine(fullName.Length);
+Console.WriteLine(phoneNumber.Length);
+
+Console.ReadKey();
