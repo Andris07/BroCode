b26c9e498a3c35120e991a06a0bcccc84b04b7e1
Andris <laczkovicsandris07.education@gmail.com>
2026-08-03 11:26:37 +0200

    Bro Code C# - 9. random numbers

---
+++ Bro Code Full Courses/C# Full Course/9. random numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿Random random = new Random();
+
+// int num = random.Next(1, 7); // random whole integer [1, 7[
+// double num = random.NextDouble(); // random decimal value [0, 1[
+
+int num1 = random.Next(1, 7);
+int num2 = random.Next(1, 7);
+int num3 = random.Next(1, 7);
+
+Console.WriteLine(num1);
+Console.WriteLine(num2);
+Console.WriteLine(num3);
+
+Console.ReadKey();
