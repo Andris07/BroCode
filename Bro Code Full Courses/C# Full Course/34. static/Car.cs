d78b528f7bc863c0323fb6262ab8a8a5046cca5c
Andris <laczkovicsandris07.education@gmail.com>
2026-08-04 21:45:39 +0200

    Bro Code C# - 34. static

---
+++ Bro Code Full Courses/C# Full Course/34. static/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _34._static
+{
+    public class Car
+    {
+        public string model;
+        public static int numberOfCars;
+
+        public Car(string model)
+        {
+            this.model = model;
+            numberOfCars++;
+        }
+
+        public static void StartRace()
+        {
+            Console.WriteLine("The race has begun!");
+        }
+    }
+}
---
+++ Bro Code Full Courses/C# Full Course/34. static/Program.cs
@@ -0,0 +1,13 @@
+﻿// static = modifier to declare a static member, which belongs to the class itself
+//          rather than to any specific object
+
+using _34._static;
+
+Car car1 = new Car("Mustang");
+Car car2 = new Car("Corvette");
+Car car3 = new Car("Lamborghini");
+
+Console.WriteLine(Car.numberOfCars);
+Car.StartRace();
+
+Console.ReadKey();
