a87431e02a33c98df357c738d393026a4a0398d5
Andris <laczkovicsandris07.education@gmail.com>
2026-08-04 21:18:29 +0200

    Bro Code C# - 31. classes

---
+++ Bro Code Full Courses/C# Full Course/31. classes/Program.cs
@@ -0,0 +1,10 @@
+﻿// class = A bundle of related code.
+//         Can be used as a blueprint to create objects (OOP)
+
+using _31._classes;
+
+Messages.Hello();
+Messages.Waiting();
+Messages.Bye();
+
+Console.ReadKey();
