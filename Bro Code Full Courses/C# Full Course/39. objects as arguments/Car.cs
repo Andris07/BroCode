8dce258eccddfce61a7e3433ded933f9f864b643
Andris <laczkovicsandris07.education@gmail.com>
2026-08-04 23:51:23 +0200

    Bro Code C# - 39. objects as arguments

---
+++ Bro Code Full Courses/C# Full Course/39. objects as arguments/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _39._objects_as_arguments
+{
+    public class Car
+    {
+        public string model;
+        public string color;
+
+        public Car(string model, string color)
+        {
+            this.model = model;
+            this.color = color;
+        }
+    }
+}
---
+++ Bro Code Full Courses/C# Full Course/39. objects as arguments/Program.cs
@@ -0,0 +1,21 @@
+﻿using _39._objects_as_arguments;
+
+Car car1 = new Car("Mustang", "red");
+Car car2 = Copy(car1);
+
+ChangeColor(car2, "silver");
+
+Console.WriteLine($"{car1.model} {car1.color}");
+Console.WriteLine($"{car2.model} {car2.color}");
+
+static void ChangeColor(Car car, string color)
+{
+    car.color = color;
+}
+
+static Car Copy(Car car)
+{
+    return new Car(car.model, car.color);
+}
+
+Console.ReadKey();
