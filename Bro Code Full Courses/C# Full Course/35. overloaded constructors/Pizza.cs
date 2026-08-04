b4355c10b27d7f2b72d69ca7473d7fd71af06b6c
Andris <laczkovicsandris07.education@gmail.com>
2026-08-04 23:19:59 +0200

    Bro Code C# - 35. overloaded constructors

---
+++ Bro Code Full Courses/C# Full Course/35. overloaded constructors/Pizza.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _35._overloaded_constructors
+{
+    public class Pizza
+    {
+        public string bread;
+        public string sauce;
+        public string cheese;
+        public string topping;
+
+        public Pizza(string bread)
+        {
+            this.bread = bread;
+        }
+
+        public Pizza(string bread, string sauce)
+        {
+            this.bread = bread;
+            this.sauce = sauce;
+        }
+
+        public Pizza(string bread, string sauce, string cheese)
+        {
+            this.bread = bread;
+            this.sauce = sauce;
+            this.cheese = cheese;
+        }
+
+        public Pizza(string bread, string sauce, string cheese, string topping)
+        {
+            this.bread = bread;
+            this.sauce = sauce;
+            this.cheese = cheese;
+            this.topping = topping;
+        }
+    }
+}
---
+++ Bro Code Full Courses/C# Full Course/35. overloaded constructors/Program.cs
@@ -0,0 +1,12 @@
+﻿// overloaded constructors = technique to create multiple constructors,
+//                           with a different set of parameters,
+//                           name + parameters = signature
+
+using _35._overloaded_constructors;
+
+Pizza pizza1 = new Pizza("stuffed crust");
+Pizza pizza2 = new Pizza("stuffed crust", "red sauce");
+Pizza pizza3 = new Pizza("stuffed crust", "red sauce", "mozzarella");
+Pizza pizza4 = new Pizza("stuffed crust", "red sauce", "mozzarella", "pepperoni");
+
+Console.ReadKey();
