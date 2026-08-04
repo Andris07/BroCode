87bf9a6adcaab9710b8d7de6075c46df1a7fad99
Andris <laczkovicsandris07.education@gmail.com>
2026-08-04 20:54:51 +0200

    Bro Code C# - 29. string interpolation

---
+++ Bro Code Full Courses/C# Full Course/29. string interpolation/Program.cs
@@ -0,0 +1,16 @@
+﻿// string interpolation = allows us to insert variables into a string literal
+//                        precede a string literal with $
+//                        {} are placeholders
+
+string firstName = "Bro";
+string lastName = "Code";
+int age = 21;
+
+//Console.WriteLine("Hello " + firstName + " " + lastName + ".");
+//Console.WriteLine("You are " + age + " years old.");
+
+Console.WriteLine($"Hello {firstName} {lastName}.");
+Console.WriteLine($"You are {age} years old.");
+// Console.WriteLine($"You are {age, 3} years old."); this would use 3 characters with right padding, using whitespaces where it wouldn't be anything, negative 3 would use left padding instead of right
+
+Console.ReadKey();
