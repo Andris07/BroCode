d46ef4cfcb4396607ecda4142054b173ed195982
Andris <laczkovicsandris07.education@gmail.com>
2026-08-04 20:06:35 +0200

    Bro Code C# - 24. return keyword

---
+++ Bro Code Full Courses/C# Full Course/24. return keyword/Program.cs
@@ -0,0 +1,21 @@
+﻿// return = returns data back to the place where a method is invoked
+
+double x;
+double y;
+double result;
+
+Console.Write("Enter in number 1: ");
+x = double.Parse(Console.ReadLine()!);
+
+Console.Write("Enter in number 2: ");
+y = double.Parse(Console.ReadLine()!);
+
+result = Multiply(x, y);
+Console.WriteLine(result);
+
+static double Multiply(double x, double y)
+{
+    return x * y;
+}
+
+Console.ReadKey();
