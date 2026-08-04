e4319f8e6690e380c6d31ec56b7c153684308441
Andris <laczkovicsandris07.education@gmail.com>
2026-08-04 20:36:33 +0200

    Bro Code C# - 28. conditional operator

---
+++ Bro Code Full Courses/C# Full Course/28. conditional operator/Program.cs
@@ -0,0 +1,21 @@
+﻿// conditional operator = used in conditional assignment if a condition is true/false
+
+// (condition) ? x : y
+
+double temperature = 20;
+string message;
+
+//if (temperature >= 15)
+//{
+//    message = "It's warm outside";
+//}
+//else
+//{
+//    message = "It's cold outside";
+//}
+
+message = (temperature >= 15) ? "It's warm outside" : "It's cold outside";
+
+Console.WriteLine(message);
+
+Console.ReadKey();
