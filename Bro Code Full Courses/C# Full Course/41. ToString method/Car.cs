42c4e64f3476ba76a05ae47b4eb72f9c338b1c6c
Andris <laczkovicsandris07.education@gmail.com>
2026-08-05 00:15:59 +0200

    Bro Code C# - ToString method

---
+++ Bro Code Full Courses/C# Full Course/41. ToString method/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _41._ToString_method
+{
+    public class Car
+    {
+        public string make;
+        public string model;
+        public int year;
+        public string color;
+
+        public Car(string make, string model, int year, string color)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.color = color;
+        }
+
+        public override string ToString()
+        {
+            return $"This is a {make} {model}";
+        }
+    }
+}
---
+++ Bro Code Full Courses/C# Full Course/41. ToString method/Program.cs
@@ -0,0 +1,9 @@
+﻿// ToString() = converts an object to its string representation so that is suitable for display
+
+using _41._ToString_method;
+
+Car car = new Car("Chevy", "Corvette", 2022, "blue");
+
+Console.WriteLine(car);
+
+Console.ReadKey();
