04cf95a761faff62977768ce431b54428be47468
Andris <laczkovicsandris07.education@gmail.com>
2026-08-04 23:44:56 +0200

    Bro Code C# - 38. array of objects

---
+++ Bro Code Full Courses/C# Full Course/38. array of objects/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _38._array_of_objects
+{
+    public class Car
+    {
+        public string model;
+
+        public Car(string model)
+        {
+            this.model = model;
+        }
+    }
+}
---
+++ Bro Code Full Courses/C# Full Course/38. array of objects/Program.cs
@@ -0,0 +1,29 @@
+﻿using _38._array_of_objects;
+
+//Car[] garage = new Car[3];
+
+//Car car1 = new Car("Mustang");
+//Car car2 = new Car("Corvette");
+//Car car3 = new Car("Lamborghini");
+
+//garage[0] = car1;
+//garage[1] = car2;
+//garage[2] = car3;
+
+Car[] garage =
+{
+    new Car("Mustang"),
+    new Car("Corvette"),
+    new Car("Lamborghini"),
+};
+
+//Console.WriteLine(garage[0].model);
+//Console.WriteLine(garage[1].model);
+//Console.WriteLine(garage[2].model);
+
+foreach (var car in garage)
+{
+    Console.WriteLine(car.model);
+}
+
+Console.ReadKey();
