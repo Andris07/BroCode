9aa064677ae05fff2564862c741ddadbb7f0f92a
Andris <laczkovicsandris07.education@gmail.com>
2026-08-04 12:42:17 +0200

    Bro Code C# - 22. foreach loop

---
+++ Bro Code Full Courses/C# Full Course/22. foreach loop/Program.cs
@@ -0,0 +1,15 @@
+﻿// foreach loop = a simpler way to iterate over an array, but it's less flexible
+
+string[] cars = { "BMW", "Mustang", "Corvette" };
+
+//for (int i = 0; i < cars.Length; i++)
+//{
+//    Console.WriteLine(cars[i]);
+//}
+
+foreach (var car in cars)
+{
+    Console.WriteLine(car);
+}
+
+Console.ReadKey();
