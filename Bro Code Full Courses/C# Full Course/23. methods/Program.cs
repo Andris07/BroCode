c0d51f9e09841066a4a097840306e1abd37a72fb
Andris <laczkovicsandris07.education@gmail.com>
2026-08-04 13:14:12 +0200

    Bro Code C# - 23. methods

---
+++ Bro Code Full Courses/C# Full Course/23. methods/Program.cs
@@ -0,0 +1,19 @@
+﻿// method = performs a section of code, whenever it's called "invoked".
+//          benefit = Let's us reuse code w/o writing it multiple times
+
+string name = "Bro";
+int age = 21;
+
+singHappyBirthday(name, age);
+
+static void singHappyBirthday(string birthdayBoy, int yearsOld)
+{
+    Console.WriteLine("Happy birthday to you!");
+    Console.WriteLine("Happy birthday to you!");
+    Console.WriteLine($"Happy birthday dear {birthdayBoy}!");
+    Console.WriteLine($"You are {yearsOld} years old!");
+    Console.WriteLine("Happy birthday to you!");
+    Console.WriteLine();
+}
+
+Console.ReadKey();
