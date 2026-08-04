7da38ad30eff6bc673656af1d2a94636c2116bd0
Andris <laczkovicsandris07.education@gmail.com>
2026-08-04 21:27:20 +0200

    Bro Code C# - 32. objects

---
+++ Bro Code Full Courses/C# Full Course/32. objects/Human.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _32._objects
+{
+    public class Human
+    {
+        public string name;
+        public int age;
+
+        public void Eat()
+        {
+            Console.WriteLine(name + " is eating");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine(name + " is sleeping");
+        }
+    }
+}
---
+++ Bro Code Full Courses/C# Full Course/32. objects/Program.cs
@@ -0,0 +1,22 @@
+﻿// object = An instance of a class
+//          A class can be used as a blueprint to create objects (OOP)
+//          objects can have fields & methods (characteristics & actions)
+
+using _32._objects;
+
+Human human1 = new Human();
+Human human2 = new Human();
+
+human1.name = "Rick";
+human1.age = 65;
+
+human2.name = "Morty";
+human2.age = 16;
+
+human1.Eat();
+human1.Sleep();
+
+human2.Eat();
+human2.Sleep();
+
+Console.ReadKey();
