63856f5ed94a3d39130638e3793f6fcf7f938f21
Andris <laczkovicsandris07.education@gmail.com>
2026-08-04 12:30:38 +0200

    Bro Code C# - 21. arrays

---
+++ Bro Code Full Courses/C# Full Course/21. arrays/Program.cs
@@ -0,0 +1,16 @@
+﻿// array = a variable that can store multiple values. fixed size
+
+// string[] cars = { "BMW", "Mustang", "Corvette" };
+
+string[] cars = new string[3];
+
+cars[0] = "Tesla";
+cars[1] = "Mustang";
+cars[2] = "Corvette";
+
+for (int i = 0; i < cars.Length; i++)
+{
+    Console.WriteLine(cars[i]);
+}
+
+Console.ReadKey();
