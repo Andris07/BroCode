6075de07c253e1f85743fd5fedc15b738f4c4be2
Andris <laczkovicsandris07.education@gmail.com>
2026-08-04 23:58:58 +0200

    Bro Code C# - 40. method overriding

---
+++ Bro Code Full Courses/C# Full Course/40. method overriding/Program.cs
@@ -0,0 +1,13 @@
+﻿// method overriding = provides a new version of a method inherited from a parent class
+//                     inherited method must be: abstract, virtual, or already overridden
+//                     Used with ToString(), polymorphism
+
+using _40._method_overriding;
+
+Dog dog = new Dog();
+Cat cat = new Cat();
+
+dog.Speak();
+cat.Speak();
+
+Console.ReadKey();
