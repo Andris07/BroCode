62471790b752b8556d300f26b1eefa9b9af268a5
Andris <laczkovicsandris07.education@gmail.com>
2026-08-05 12:11:57 +0200

    Bro Code C# - 46. getters & setters

---
+++ Bro Code Full Courses/C# Full Course/46. getters & setters/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _46._getters___setters
+{
+    public class Car
+    {
+        private int speed;
+        public int Speed
+        {
+            get
+            {
+                return speed;
+            }
+            set
+            {
+                if (value > 500)
+                {
+                    speed = 500;
+                }
+                else
+                {
+                    speed = value;
+                }
+            }
+        }
+
+        public Car(int speed)
+        {
+            this.Speed = speed;
+        }
+    }
+}
---
+++ Bro Code Full Courses/C# Full Course/46. getters & setters/Program.cs
@@ -0,0 +1,15 @@
+﻿// getters & setters = add security to fields by encapsulation
+//                     They're accessors found within properties
+
+// properties = combine aspects of both fields and methods (share name with a field)
+// get accessor = used to return the property value
+// set accessor = used to assign a new value
+// value keyword = defines the value being assigned by the set (parameter)
+
+using _46._getters___setters;
+
+Car car = new Car(400);
+
+Console.WriteLine(car.Speed);
+
+Console.ReadKey();
