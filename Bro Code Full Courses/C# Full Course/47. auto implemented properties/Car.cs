0af811fa19192f68e9f06a390eaad42654fb4db4
Andris <laczkovicsandris07.education@gmail.com>
2026-08-05 12:16:42 +0200

    Bro Code C# - 47. auto implemented properties

---
+++ Bro Code Full Courses/C# Full Course/47. auto implemented properties/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _47._auto_implemented_properties
+{
+    public class Car
+    {
+        public string Model { get; set; }
+
+        public Car(string model)
+        {
+            this.Model = model;
+        }
+    }
+}
---
+++ Bro Code Full Courses/C# Full Course/47. auto implemented properties/Program.cs
@@ -0,0 +1,11 @@
+﻿// Auto-Implemented properties = shortcut when no additional logic is required in the property
+//                               you do not have to define a field for a property,
+//                               you only have to write get; and/or set; inside the property
+
+using _47._auto_implemented_properties;
+
+Car car = new Car("Porsche");
+
+Console.WriteLine(car.Model);
+
+Console.ReadKey();
