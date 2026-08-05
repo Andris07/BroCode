f24ca7e9c2be985c805ad649876342687b6d4451
Andris <laczkovicsandris07.education@gmail.com>
2026-08-05 13:27:45 +0200

    Bro Code C# - 49. generics

---
+++ Bro Code Full Courses/C# Full Course/49. generics/Program.cs
@@ -0,0 +1,22 @@
+﻿// generic = "not specific to a particular data type"
+//           add <T> to: classes, methods, fields, etc.
+//           allows for code reusability for different data types
+
+int[] intArray = { 1, 2, 3 };
+double[] doubleArray = { 1.0, 2.0, 3.0 };
+string[] stringArray = { "1", "2", "3" };
+
+displayElements(intArray);
+displayElements(doubleArray);
+displayElements(stringArray);
+
+Console.ReadKey();
+
+static void displayElements<Thing>(Thing[] array)
+{
+    foreach (var item in array)
+    {
+        Console.Write($"{item} ");
+    }
+    Console.WriteLine();
+}
