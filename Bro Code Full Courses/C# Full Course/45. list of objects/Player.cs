5c38b865c544bef7a0bbd5af91b6a6ccc789284f
Andris <laczkovicsandris07.education@gmail.com>
2026-08-05 11:39:31 +0200

    Bro Code C# - 45. list of objects

---
+++ Bro Code Full Courses/C# Full Course/45. list of objects/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _45._list_of_objects
+{
+    public class Player
+    {
+        public string username;
+
+        public Player(string username)
+        {
+            this.username = username;
+        }
+
+        public override string ToString()
+        {
+            return username;
+        }
+    }
+}
---
+++ Bro Code Full Courses/C# Full Course/45. list of objects/Program.cs
@@ -0,0 +1,14 @@
+﻿using _45._list_of_objects;
+
+List<Player> players = new List<Player>();
+
+players.Add(new Player("Chad"));
+players.Add(new Player("Steve"));
+players.Add(new Player("Karen"));
+
+foreach (var player in players)
+{
+    Console.WriteLine(player);
+}
+
+Console.ReadKey();
